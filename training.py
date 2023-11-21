f1d75dd74449cb538f5191295765b9c3bc57245d
Devang Chaudhary <devangc@umich.edu>
2023-11-20 19:11:13 -0500

    Further work on trasformer

--- training.py
+++ training.py
@@ -5,7 +5,7 @@ from transformers import AutoTokenizer,AdamW,DistilBertModel
 from torch.nn import CrossEntropyLoss
 from torch.utils.data import DataLoader
 from tqdm import tqdm
-from data_process import *
+from clustering import *
 
 class StanceDetect(nn.Module):
     def __init__(self, distilBert: DistilBertModel, num_pos: int, hidden_dims: list(int)):
@@ -19,18 +19,28 @@ class StanceDetect(nn.Module):
         super().__init__()
         
         self.distilbert = distilBert
-        self.fc = nn.Linear(hidden_dims[0],hidden_dims[1])
-        self.classifier = nn.Linear(hidden_dims[1],num_pos)
-    #     __init_weights__(hidden_dims)
+        self.classifier = nn.Sequential(
+            nn.Linear(768, 768),
+            nn.ReLU(),
+            nn.Dropout(0.3), # For regularization
+            nn.Linear(768,num_pos) # classification)
+        )
         
-    # def __init_weights__(self, hidden_dims: list(int)):
-    #     # torch.seed(42)
-    #     torch.random.normal(self.fc._weights,)
-    def forward():
-        pass
+    def forward(self,input_ids,attention_mask):
+        bert = self.distilbert(input_ids=input_ids,attention_mask=attention_mask)
+        output = self.classifier(bert.last_hidden_state)
+        return output
         
+
+def train(model, train, val):
+    
 
 if __name__ == "main":
     
-    train_lab, train_unlab, test = process()
-    
+    x_train, y_train, x_test, y_test = cluster_then_label()
+    # concatenate x_train and y_train to pass into DataLoader
+    train = DataLoader()
+    hidden_dim = 100 #TODO: Change to reasonable number , maybe based on dataset vocab or something else
+    bert_model = DistilBertModel.from_pretrained('distilbert-base-uncased')
+    model = StanceDetect(bert_model, 2)
+    train(model,x_train,y_train)
